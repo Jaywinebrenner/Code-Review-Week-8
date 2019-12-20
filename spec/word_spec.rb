3d782fcde9458922f08dd115b3a2b7f1c5955dd6
Jay Winebrenner <jaywinebrenner@gmail.com>
2019-12-20 08:22:41 -0800

    add test for the same word if it has the same attributes as another word

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -7,3 +7,13 @@ describe '#Word' do
     Word.clear()
     # Definition.clear()
   end
+
+
+  describe('#==') do
+      it("is the same word if it has the same attributes as another word") do
+        word = Word.new("Nihilism", nil)
+        word2 = Word.new("Nihilism", nil)
+        expect(word).to(eq(word2))
+      end
+    end
+end
